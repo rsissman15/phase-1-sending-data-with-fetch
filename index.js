4463a4c76db61d67e0889bf434a2d6adf8e06508
Rebecca Sissman <rsissman15@aol.com>
2022-01-25 18:29:01 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+
+
+const body=document.querySelector("body");
+
+function submitData(firstName,email){
+    const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          name: firstName,
+          email: email
+        }),
+      };
+      return fetch("http://localhost:3000/users",configurationObject)
+      .then(res=>res.json())
+      .then(body=>addToBody(body.id))
+      .catch(err=>{
+        alert("Unathorized Accesss")
+        body.append(err.message)
+      })
+      
+}
+
+function addToBody(newId){
+    body.innerHTML=newId;
+    //body.append(err.message);
+}
+
